59d8b0cdb42a81074876a0fcdd439fc4e2e8e8c8
bagusubash <102565280+bagusubash@users.noreply.github.com>
2022-03-28 21:27:20 -0400

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,16 @@
+print("calculator")
+
+x = 6
+y = 8
+
+sum = x + y
+difference = x - y 
+product = x * y
+quotient = x / y
+
+print("the numbers are,", x, "and ", y)
+print("and ", y)
+print(" the sum is ", sum)
+print("the difference is",difference)
+print("the product is ", product)
+print("the quotient is ", quotient)
